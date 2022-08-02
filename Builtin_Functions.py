5a75b20d1e60338c8c6f6a92d7e6912ce37193e7
Niharika186 <107666477+Niharika186@users.noreply.github.com>
2022-08-02 14:28:59 +0530

    Add files via upload

---
+++ Builtin_Functions.py
@@ -0,0 +1,20 @@
+'''sorted'''
+# a=[1,2,3]
+# a.sort()
+# print(a)     #  [1, 2, 3]
+# a.sort(reverse=True)
+# print(a)     #  [3, 2, 1]
+
+'''all-if the function has empty string or false or 0 or none it shows false'''
+# print(all([True,1,0]))  # False
+# print(all([False,1,True]))  # False
+# print(all([True,1,' ']))  # True
+# print(all([True,1,' ',None]))  # False
+# print(all([True,1,""]))  # False
+
+'''any- if the function has true it show the o/p true'''
+# print(any([False,0,1]))      # True
+# print(any([False,0,None]))   #  False
+# print(any([False,0,True]))   # True
+# print(any([False,0]))        #  False
+
